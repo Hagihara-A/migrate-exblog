a13c79af583d967f0fa801ce29507d89cee14266
Hagihara-A <knkr5456@gmail.com>
2019-02-20 23:36:19 +0900

    made it be able to  install via pip

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
-setup(name='scrape-excite-blog',
+
+with open('README.md', 'r') as f:
+    long_descripton = f.read()
+
+setup(name='migrate-excite-blog',
       version='1.0.0',
       packages=find_packages(),
+      long_descripton=long_descripton,
+      long_descripton_content_type='',
+      python_requires='>=3.4',
       include_package_data=True,
       install_requires=[
           'requests',
